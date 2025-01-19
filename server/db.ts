67d2f2d3aed43008475a547d983a50f81c140bab
Mustafa Hanif <mustafa.hanif@seera.sa>
2025-01-19 19:00:28 +0400

    m

--- server/db.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from 'bun:sqlite'
-import { drizzle } from 'drizzle-orm/bun-sqlite'
-import * as schema from './schema/schema'
-import path from 'path'
-
-const sqlite = new Database(path.resolve(__dirname, '../data/data.db'))
-export const db = drizzle(sqlite, { schema })
